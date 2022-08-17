9ec5b7e21966415fe8203c58fe27b89d4cb13abe
janvimodha <janvimodha.encircle@gmail.com>
2022-08-17 18:58:00 +0530

    hello

---
+++ assets/demo.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   $(document).ready(function(){
+	          $('#slider1').slick({
+	              dots: true,
+	              arrows: false, 
+	              autoplay: true,
+	              autoplaySpeed: 2000
+	          }) 
+	       });
+});
